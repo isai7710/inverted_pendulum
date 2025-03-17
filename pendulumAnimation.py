93847896d06a154f5d8e3608aacd923d6d461126
Isai Sanzy <isai10sanchez@gmail.com>
2025-03-17 16:15:48 -0600

    struggled to redraw the animation with a circular rod as the pendulum since that is more close to what we are modeling

--- pendulumAnimation.py
+++ pendulumAnimation.py
@@ -1,37 +1,17 @@
-# ----------------------
-# Date: 6/8/2023
-# Name: Isai Sanchez
-# Purpose: Develop python class to animate a dynamic system, in this case, an inverted pendulum on cart
-# (inspiration and some code from BYU Introduction to Feedback Control book)
-# Description: inverted pendulum on cart
-# ----------------------
-
 import numpy as np
 from matplotlib import patches as mpatches
 from matplotlib import pyplot as plt
+from matplotlib import transforms
 
 import pendulumParam as P
-
-# if you are having difficulty with the graphics,
-# try using one of the following backends
-# See https://matplotlib.org/stable/users/explain/backends.html
-# import matplotlib
-# matplotlib.use('qtagg')  # requires pyqt or pyside
-# matplotlib.use('ipympl')  # requires ipympl
-# matplotlib.use('gtk3agg')  # requires pyGObject and pycairo
-# matplotlib.use('gtk4agg')  # requires pyGObject and pycairo
-# matplotlib.use('gtk3cairo')  # requires pyGObject and pycairo
-# matplotlib.use('gtk4cairo')  # requires pyGObject and pycairo
-# matplotlib.use('tkagg')  # requires TkInter
-# matplotlib.use('wxagg')  # requires wxPython
 
 
 class pendulumAnimation:
     def __init__(self):
-        self.flag_init = True  # Used to indicate initialization
+        self.flag_init = True  # Flag used to indicate initialization
         # Initialize a figure and axes object
         self.fig, self.ax = plt.subplots()
-        # Initializes a list of objects (patches and lines)
+        # Initializes a list of objects (matplotlib patches objects used for animation drawings)
         self.handle = []
         # Specify the x,y axis limits
         plt.axis([-3 * P.ell, 3 * P.ell, -0.1, 3 * P.ell])
@@ -40,25 +20,32 @@ class pendulumAnimation:
         # label axes
         plt.xlabel("z")
 
+        # Set pendulum rod properties
+        self.rod_width = 0.1  # Width of the pendulum rod
+
+        # Cross hair properties
+        self.cross_size = 0.01  # Size of the cross hair
+        self.cross_color = "green"  # Color of the cross hair
+
     def update(self, state):
         theta = state[0][0]  # Angle of pendulum, rads
         z = state[1][0]  # Horizontal position of cart, m
-        # draw plot elements: cart, bob, rod
+        # draw plot elements: cart, rod, and cross hair
         self.draw_cart(z)
-        self.draw_bob(z, theta)
         self.draw_rod(z, theta)
+        self.draw_wheels(z)
         self.ax.axis("equal")
         # Set initialization flag to False after first call
-        if self.flag_init == True:
+        if self.flag_init:
             self.flag_init = False
 
     def draw_cart(self, z):
         # specify bottom left corner of rectangle
         x = z - P.w / 2.0
-        y = P.gap
+        y = P.wheel_radius
         corner = (x, y)
         # create rectangle on first call, update on subsequent calls
-        if self.flag_init is True:
+        if self.flag_init:
             # Create the Rectangle patch and append its handle
             # to the handle list
             self.handle.append(
@@ -69,36 +56,65 @@ class pendulumAnimation:
         else:
             self.handle[0].set_xy(corner)  # Update patch
 
-    def draw_bob(self, z, theta):
-        # specify center of circle
-        x = z + (P.ell + P.radius) * np.sin(theta)
-        y = P.gap + P.h + (P.ell + P.radius) * np.cos(theta)
-        center = (x, y)
+    def draw_rod(self, z, theta):
+        """
+        Draw the pendulum as a rod with round ends attached to the center of the cart.
+        Uses a thick line with round caps instead of a rectangle.
+        """
+        # The pivot point is at the center of the cart
+        pivot_x = z
+        pivot_y = P.wheel_radius + P.h / 2  # Center of the cart
+
+        # Calculate the end point of the rod
+        end_x = pivot_x + P.ell * np.sin(theta)
+        end_y = pivot_y + P.ell * np.cos(theta)
+
+        if self.flag_init:
+            # Create a thick line with round caps
+            (rod,) = self.ax.plot(
+                [pivot_x, end_x],
+                [pivot_y, end_y],
+                linewidth=self.rod_width * 50,  # Scale up for visibility
+                color="saddlebrown",
+                solid_capstyle="round",  # This gives the round ends
+            )
+
+            # Add the rod to the handle list
+            self.handle.append(rod)
+        else:
+            # Update the line data points
+            self.handle[1].set_xdata([pivot_x, end_x])
+            self.handle[1].set_ydata([pivot_y, end_y])
+
+    def draw_wheels(self, z):
+        # specify center of wheels
+        left_wheel_center = (z - P.w / 2.0, P.wheel_radius)
+        right_wheel_center = (z + P.w / 2.0, P.wheel_radius)
         # create circle on first call, update on subsequent calls
         if self.flag_init is True:
             # Create the CirclePolygon patch and append its handle
             # to the handle list
-            self.handle.append(
-                mpatches.CirclePolygon(
-                    center, radius=P.radius, resolution=15, fc="limegreen", ec="black"
-                )
+            left_wheel = mpatches.CirclePolygon(
+                left_wheel_center,
+                radius=P.wheel_radius,
+                resolution=15,
+                fc="limegreen",
+                ec="black",
             )
-            # Add the patch to the axes
-            self.ax.add_patch(self.handle[1])
-        else:
-            self.handle[1].xy = center
+            self.handle.append(left_wheel)
+            self.ax.add_patch(left_wheel)
 
-    def draw_rod(self, z, theta):
-        # specify x-y points of the rod
-        X = [z, z + P.ell * np.sin(theta)]
-        Y = [P.gap + P.h, P.gap + P.h + P.ell * np.cos(theta)]
-        # create rod on first call, update on subsequent calls
-        if self.flag_init is True:
-            # Create the line object and append its handle
-            # to the handle list.
-            (line,) = self.ax.plot(X, Y, lw=1, c="black")
-            self.handle.append(line)
+            right_wheel = mpatches.CirclePolygon(
+                right_wheel_center,
+                radius=P.wheel_radius,
+                resolution=15,
+                fc="limegreen",
+                ec="black",
+            )
+            self.handle.append(right_wheel)
+            self.ax.add_patch(right_wheel)
         else:
-            self.handle[2].set_xdata(X)
-            self.handle[2].set_ydata(Y)
-
+            # current handle list has the following indeces
+            # cart is 0, rod is 1, left wheel is 2, right wheel is 3
+            self.handle[2].xy = left_wheel_center
+            self.handle[3].xy = right_wheel_center
